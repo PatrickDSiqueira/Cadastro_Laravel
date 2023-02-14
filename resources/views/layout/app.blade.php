42a024a877505079030114c3cfe92523b720c36c
Patrick Siqueira <psique.estuda@gmail.com>
2023-02-14 09:22:17 -0300

    incrementando mais um pouco o index

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,6 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="-token" content="{{csrf_token()}}" >
+    <style>
+        body{
+            padding: 20px;
+        }
+    </style>
 
     <title>Cadastro de Produtos</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
