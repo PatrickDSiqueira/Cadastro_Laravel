877057befe0b36b627dc8df79f50f5f9c0d7d83f
Patrick Siqueira <psique.estuda@gmail.com>
2023-02-14 11:16:07 -0300

    Criando controladores

---
+++ resources/views/categorias.blade.php
@@ -0,0 +1,4 @@
+@extends('layout.app')
+@section('body')
+    <h4>PAgina Categorias</h4>
+@endsection
---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,4 @@
+@extends('layout.app')
+@section('body')
+    <h4>PAgina produtos</h4>
+@endsection
