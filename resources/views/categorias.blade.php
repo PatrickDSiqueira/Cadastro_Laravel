1292145d2b76b86a19a2d162fa690090873c4864
Patrick Siqueira <psique.estuda@gmail.com>
2023-02-14 14:09:55 -0300

    Lsitando categorias

--- resources/views/categorias.blade.php
+++ resources/views/categorias.blade.php
@@ -1,4 +1,35 @@
 @extends('layout.app', ["current" => "categorias"])
 @section('body')
-    <h4>PAgina Categorias</h4>
+    <div class="card border">
+        <div class="card-body">
+            <h5 class="card-title">Cadastro de Categorias</h5>
+            @if(count($cats)>0)
+
+                <table class="table table-bordered table-hover">
+                    <thead>
+                    <tr>
+                        <th>Código</th>
+                        <th>Nome da Categoria</th>
+                        <th>Ações</th>
+                    </tr>
+                    </thead>
+                    <body>
+                    @foreach($cats as $cat)
+                        <tr>
+                            <td>{{$cat->id}}</td>
+                            <td>{{$cat->name}}</td>
+                            <td>
+                                <a href="/categorias/editar{{$cat->id}}" class="btn btn-sm btn-primary">Editar</a>
+                                <a href="/categorias/apagar{{$cat->id}}" class="btn btn-sm btn-danger">Deletar</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </body>
+                </table>
+            @endif
+        </div>
+        <div class="card-footer">
+            <a href="/categorias/novo" class="btn btn-sm btn-primary" role="button">Nova Categoria</a>
+        </div>
+    </div>
 @endsection
